640c0a48ca131d0ff13cb58b3ff82250ac303d34
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-19 17:37:09 +0200

    frontend: implementing main frontend of app

--- frontend/components/AddTask.tsx
+++ frontend/components/AddTask.tsx
@@ -1,1 +1,13 @@
+import axios from "axios"
+import { useState } from "react"
 
+export default async function AddTask() {
+    const [titile, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [due_date, setDue_date] = useState("");
+    try {
+        const res = axios.post('http://localhost:5000/task/add', {
+
+        })
+    }
+}
