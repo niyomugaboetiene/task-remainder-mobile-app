c6f99ad27fa27ddb686938dafe172144e86bea14
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-19 14:33:18 +0200

    backend: start implementation

---
+++ backend/conn.js
@@ -0,0 +1,13 @@
+import mysql from "mysql2"
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'task-remainder-app'
+})
+
+connection.connect((err) => {
+    if (err) console.log("ERROR:", err)
+    console.log("Connected successfully");    
+});
