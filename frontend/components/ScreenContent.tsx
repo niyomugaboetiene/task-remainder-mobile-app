ac936400ff390bb927f0dd1acfd06f55d8215876
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-19 17:54:07 +0200

    frontend: implementing main frontend of app

--- frontend/components/ScreenContent.tsx
+++ frontend/components/ScreenContent.tsx
@@ -4,17 +4,12 @@ import { Text, View } from 'react-native';
 import { EditScreenInfo } from './EditScreenInfo';
 
 type ScreenContentProps = {
-  title: string;
-  path: string;
   children?: React.ReactNode;
 };
 
-export const ScreenContent = ({ title, path, children} : ScreenContentProps) => {
+export const ScreenContent = ({ children} : ScreenContentProps) => {
   return (
     <View className={styles.container}>
-      <Text className={styles.title}>{title}</Text>
-      <View className={styles.separator} />
-      <EditScreenInfo path={path} />
       {children}
     </View>
   );
