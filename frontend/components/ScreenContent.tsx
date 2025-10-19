189caf44a7cf1e88b20d1daa3318afeca6c9be84
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-19 18:05:39 +0200

    frontend: implementing main frontend of app

--- frontend/components/ScreenContent.tsx
+++ frontend/components/ScreenContent.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-import { EditScreenInfo } from './EditScreenInfo';
 
 type ScreenContentProps = {
   children?: React.ReactNode;
