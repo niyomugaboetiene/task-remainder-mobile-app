8716716b49efe6529af503ea7d80c9681463f11f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-19 18:08:59 +0200

    frontend: implementing main frontend of app

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,7 +2,7 @@ import { Stack, Link } from 'expo-router';
 
 import { View } from 'react-native';
 
-
+import AddTask from '@/components/AddTask';
 import { Container } from '@/components/Container';
 import { ScreenContent } from '@/components/ScreenContent';
 
@@ -12,7 +12,11 @@ export default function Home() {
   return (
     
       <View className='flex-1'>
-
+         <Container>
+            <ScreenContent>
+                 <AddTask />
+            </ScreenContent>
+         </Container>
     </View>
   );
 }
