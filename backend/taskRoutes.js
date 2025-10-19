f226154e5cc961a3fef89dde453e406c2fa2e90d
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-19 16:58:53 +0200

    backend: implementing main backend of app

--- backend/taskRoutes.js
+++ backend/taskRoutes.js
@@ -6,9 +6,11 @@ route.post('/add', (req, res) => {
     const { name, description, due_date, status } = req.body;
     if (!name || !description || !due_date, !status) {
        const sql = "INSERT INTO tasks(name, description, due_date, status, created_at) VALUES(?, ?, ?, ?, ?)";
-       connection.query(sql, [name, description, due_date,status, new Date()], (err) => {
-            if (err) console.log("ERROR", err)
-            console.log("Inserted successfully")
+       connection.query(sql, [name, description, due_date,status, new Date()], (err, result) => {
+          if (err) {
+            console.error(err, "Database error")
+            return res.status(500).json("Database error");
+          }
        });
     }
 
