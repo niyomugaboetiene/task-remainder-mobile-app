a8b468f916839c9fa9615e05dffa0baf99d7320e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-19 16:55:05 +0200

    backend: implementing main backend of app

--- backend/taskRoutes.js
+++ backend/taskRoutes.js
@@ -4,11 +4,14 @@ const route = express.Router()
 
 route.post('/add', (req, res) => {
     const { name, description, due_date, status } = req.body;
-    const sql = "INSERT INTO tasks(name, description, due_date, status, created_at) VALUES(?, ?, ?, ?, ?)";
-    connection.query(sql, [name, description, due_date,status, new Date()], (err) => {
-        if (err) console.log("ERROR", err)
-        console.log("Inserted successfully")
-    });
+    if (!name || !description || !due_date, !status) {
+       const sql = "INSERT INTO tasks(name, description, due_date, status, created_at) VALUES(?, ?, ?, ?, ?)";
+       connection.query(sql, [name, description, due_date,status, new Date()], (err) => {
+            if (err) console.log("ERROR", err)
+            console.log("Inserted successfully")
+       });
+    }
+
 })
 
 export default route
