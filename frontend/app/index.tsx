3936b5aff756a52772b1e82ab951febf1cce1d44
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-19 18:07:57 +0200

    frontend: implementing main frontend of app

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -3,7 +3,6 @@ import { Stack, Link } from 'expo-router';
 import { View } from 'react-native';
 
 
-import { Button } from '@/components/Button';
 import { Container } from '@/components/Container';
 import { ScreenContent } from '@/components/ScreenContent';
 
@@ -12,19 +11,8 @@ import { ScreenContent } from '@/components/ScreenContent';
 export default function Home() {
   return (
     
-      <View className={styles.container}>
-    
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home">
-        
-        </ScreenContent>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          
-              <Button title="Show Details" />
-          
-        </Link>
-      </Container>
+      <View className='flex-1'>
+
     </View>
   );
 }
