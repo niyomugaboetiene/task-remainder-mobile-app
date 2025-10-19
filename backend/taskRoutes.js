c34ae6e92750aa57ccedfe9d2e3dadeb2e02d0af
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-19 17:01:46 +0200

    backend: implementing main backend of app

--- backend/taskRoutes.js
+++ backend/taskRoutes.js
@@ -3,10 +3,10 @@ import connection from "./conn.js";
 const route = express.Router()
 
 route.post('/add', (req, res) => {
-    const { name, description, due_date, status } = req.body;
-    if (!name || !description || !due_date, !status) {
-       const sql = "INSERT INTO tasks(name, description, due_date, status, created_at) VALUES(?, ?, ?, ?, ?)";
-       connection.query(sql, [name, description, due_date,status, new Date()], (err, result) => {
+    const { title, description, due_date, status } = req.body;
+    if (!title || !description || !due_date, !status) {
+       const sql = "INSERT INTO tasks(title, description, due_date, status, created_at) VALUES(?, ?, ?, ?, ?)";
+       connection.query(sql, [title, description, due_date,status, new Date()], (err, result) => {
           if (err) {
             console.error(err, "Database error")
             return res.status(500).json("Database error");
