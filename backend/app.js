93738790fabb3dbb649347b0104339c7e7b1a2fb
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-20 07:48:43 +0200

    backend: implementing about add task

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 import express from "express"
+import cors from "cors"
 import taskRoute from "./taskRoutes.js"
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:8081',
+    credentials: true
+}))
 app.use('/task', taskRoute);
 
 app.listen(5000, () => {
