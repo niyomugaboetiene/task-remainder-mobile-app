935a9d2816f2c3ecf250587e696aef1a722985cd
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-20 08:24:18 +0200

    implementing about adding task

--- frontend/components/AddTask.tsx
+++ frontend/components/AddTask.tsx
@@ -15,7 +15,7 @@ export default  function AddTask() {
        try {
            setLoading(true)
            setError("")
-           await axios.post('http://10.0.2.2:5000/task/add', {
+           await axios.post('http://localhost:5000/task/add', {
               titile, description, due_date, status, createdAt
            }, {
             headers: {
