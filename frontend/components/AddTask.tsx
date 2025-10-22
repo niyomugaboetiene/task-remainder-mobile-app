6f2e5749bedc955739c3605e8c63a7e7df64e730
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-22 20:12:18 +0200

    react-native: fixing bug in add task tab

--- frontend/components/AddTask.tsx
+++ frontend/components/AddTask.tsx
@@ -41,9 +41,9 @@ export default  function AddTask() {
     }
  
     return (
-         <View className="min-h-screen bg-gradient-to-bl from-blue-300 to-gray-300 via-green-300">
-            <Text>Add Task</Text>
-            <Text>Title</Text>
+         <View className="min-h-screen bg-gradient-to-bl from-blue-300 to-gray-300 via-green-300 p-4">
+               <Text className="text-xl">Add Task</Text>
+             <Text>Title</Text>
             <TextInput 
                 
                placeholder="Enter title"
