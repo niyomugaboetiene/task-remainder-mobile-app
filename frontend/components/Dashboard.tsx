c381180b9cad151054a75c538f45cc391697b382
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-23 18:18:45 +0200

    react-natived: fixing navigation

--- frontend/components/Dashboard.tsx
+++ frontend/components/Dashboard.tsx
@@ -28,6 +28,7 @@ function Dashboard(){
         const hour = new Date().getHours();
         if (hour < 12) return "Good Moring";
         if (hour < 18) return "Good Afternoon";
+        return "Good Evening";
     }
 
     return (
