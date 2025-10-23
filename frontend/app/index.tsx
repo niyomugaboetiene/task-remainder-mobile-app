707376b4797e0b952c05d6bbbff605eeb3270a57
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-23 11:48:07 +0200

    backend: fetching user session info

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -6,10 +6,11 @@ import AddTask from '@/components/AddTask';
 import { Container } from '@/components/Container';
 import SignIn from '@/components/SignIn';
 import { ScreenContent } from '@/components/ScreenContent';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Dashboard from '@/components/Dashboard';
 import SignUp from '@/components/SignUp';
 
-
+const Tab = createBottomTabNavigator();
 
 export default function Home() {
   return (
@@ -17,10 +18,16 @@ export default function Home() {
       <View className='flex-1'>
          <Container>
             <ScreenContent>
-                 {/* <AddTask /> */}
-                 {/* <SignIn /> */}
-                 {/* <SignUp /> */}
-                 <Dashboard />
+              <Tab.Navigator
+                 screenOptions={{
+                  headerShown: false
+                 }}
+               >
+                    <Tab.Screen name="Home" component={Dashboard} />
+                    <Tab.Screen name="Sign-in" component={SignIn} />
+                    <Tab.Screen name="Sign-up" component={SignUp} />
+                    <Tab.Screen name="Add-Task" component={AddTask} />
+              </Tab.Navigator>
             </ScreenContent>
          </Container>
     </View>
