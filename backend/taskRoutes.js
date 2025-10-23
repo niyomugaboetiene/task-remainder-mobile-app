f9c46a331fab24feb13d8df7605f81d52da1dc3c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-23 08:16:57 +0200

    backend: creating account  in the database

--- backend/taskRoutes.js
+++ backend/taskRoutes.js
@@ -21,7 +21,7 @@ route.post('/sign-in', async (req, res) => {
 route.get('/sign-in', async (req, res) => {
    const { username, email, password } = req.body;
    const sql = "SELECT * FROM users WHERE username = ?, password = ?, email = ?";
-   const comparedPassword = bcrypt.compare(password, )
+   const comparedPassword = bcrypt.compare(password)
    if (!username || !email || !password) {
     connection.query(sql, [])
    }
