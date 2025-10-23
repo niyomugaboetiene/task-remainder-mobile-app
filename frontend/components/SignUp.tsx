aac769a2596ab7928f9635b10299d07614f49c7f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-23 11:12:09 +0200

    react-native: sign-in  in frontend

--- frontend/components/SignUp.tsx
+++ frontend/components/SignUp.tsx
@@ -10,10 +10,7 @@ function SignUp() {
        const [error, setError] = useState("");
 
        const Login = async () => {
-          const res = await axios.post('http://localhost:5000/sign-in', {
-            username, email, password
-          }, {
-            withCredentials: true
-          })
+          const res = await axios.post('http://localhost:5000/sign-in', { username, email, password }, { withCredentials: true  });
+          
        }
 }
