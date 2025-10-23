c7ba76213e242015ede2a0022e6ae858c0898cad
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-23 17:08:49 +0200

    react-natived: fixing navigation

--- frontend/components/Dashboard.tsx
+++ frontend/components/Dashboard.tsx
@@ -1,16 +1,32 @@
 import { View, Text, TouchableOpacity } from "react-native"
 import axios from "axios"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function Dashboard(){
     const [userInfo, setUserInfo] = useState("");
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+ 
+    useEffect(() => {
+       const UserInfo = async() => {
+        try {
+            setLoading(true)
+            const res = await axios.get('http://localhost:5000/loggedIn', {withCredentials: true});
+            setUserInfo(res.data.loggedIn);
+            setLoading(true);
+        } catch (err: any) {
+            setError(err?.response?.data);
+            setLoading(false);
+        } finally {
+            setLoading(false);
+        }
 
-    const res: any = axios.get('http://localhost:5000/loggedIn', {withCredentials: true});
-    setUserInfo(res);
+       }
+    })
+  
 
     return (
         <View>
-            <Text>Welcome {userInfo}</Text>
         </View>
     )
 }
