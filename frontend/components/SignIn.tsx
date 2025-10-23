4cb3ad1ff8bb4eded47e85058cd3f491a351623e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-23 10:11:58 +0200

    react-native: creating account  using frontend

---
+++ frontend/components/SignIn.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+import { useState } from "react"
+
+async function SignIn() {
+   const [full_name, setFull_name] = useState("");
+   const [username, setUsername] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+}
