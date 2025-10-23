3dbb922d00dd57bd1c1ca26f73146e6f2ff0ab8b
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-23 11:10:31 +0200

    react-native: sign-in  in frontend

---
+++ frontend/components/SignUp.tsx
@@ -0,0 +1,15 @@
+import { View, Text, } from "react-native"
+import axios from "axios"
+import { useState } from "react";
+
+function SignUp() {
+       const [username, setUsername] = useState("");
+       const [email, setEmail] = useState("");
+       const [password, setPassword] = useState("");
+       const [success, setSuccess] = useState("");
+       const [error, setError] = useState("");
+
+       const Login = async () => {
+          const res = await axios.post()
+       }
+}
