92398c7a103ccdc472dcf958c1ffe0114cdc5857
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-23 08:00:50 +0200

    backend: creating account  in the database

--- backend/taskRoutes.js
+++ backend/taskRoutes.js
@@ -5,7 +5,7 @@ const route = express.Router();
 
 route.post('/sign-in', (req, res) => {
    const { full_name, username, email, password } = req.body;
-   const sql = "INSERT INTO users(username, email, password) VALUES(?, ?, ?)";
+   const sql = "INSERT INTO users(full_name, username, email, password) VALUES(?, ?, ?)";
    connection.query(sql, [full_name, username, email, password], (err) => {
     if (err) {
       return res.status(500).json({ error: err.message });
