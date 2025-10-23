32feffeb97d99dbf0aae92c4c12bd30d4b134fbd
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-23 15:28:48 +0200

    react-natived: fixing navigation

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,46 +1,59 @@
-import { Stack, Link } from 'expo-router';
-import { Settings, View } from 'react-native';
+import { View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 import AddTask from '@/components/AddTask';
 import { Container } from '@/components/Container';
 import SignIn from '@/components/SignIn';
 import { ScreenContent } from '@/components/ScreenContent';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Dashboard from '@/components/Dashboard';
 import SignUp from '@/components/SignUp';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 
 const Tab = createBottomTabNavigator();
 
-export default function Home() {  
-  const [loggin, setLoggin] = useState(false);
+export default function Home() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   useEffect(() => {
-   const checkLogin = async() => {
-    await axios.get('http://localhost:5000/loggedIn', { withCredentials: true });
-     setLoggin(true);
-   }
-   checkLogin();
+    const checkLogin = async () => {
+      try {
+        const res = await axios.get('http://localhost:5000/loggedIn', { withCredentials: true });
+        if (res.data.loggedIn) {
+          setLoggedIn(true);
+        } else {
+          setLoggedIn(false);
+        }
+      } catch (err) {
+        setLoggedIn(false);
+      }
+    };
+    checkLogin();
   }, []);
 
   return (
-         <View className='flex-1'>
-             <Container>
-                <ScreenContent>
-                  {loggin ? (
-                    <Tab.Navigator initialRouteName='Sign-in'>
-                        <Tab.Screen name='Home' component={Dashboard}/>
-                        <Tab.Screen name='Add-task' component={AddTask}/>
-                        <Tab.Screen name='Your-task' component={Dashboard}/>
-                        <Tab.Screen name='Settings' component={Dashboard}/>
-                    </Tab.Navigator>
-                  ) : (
-                    <Tab.Navigator>
-                        <Tab.Screen name='Sign-in' component={SignIn}/>
-                        <Tab.Screen name='Sign-up' component={SignUp}/>
-                    </Tab.Navigator>
-                  )}
-                </ScreenContent>
-                  </Container>
-          </View>  
-);}
+    <View className="flex-1">
+      <Container>
+        <ScreenContent>
+          {loggedIn ? (
+            <Tab.Navigator
+              initialRouteName="Home"
+              screenOptions={{
+                headerShown: false,
+              }}
+            >
+              <Tab.Screen name="Home" component={Dashboard} />
+              <Tab.Screen name="Add-task" component={AddTask} />
+              <Tab.Screen name="Your-task" component={Dashboard} />
+              <Tab.Screen name="Settings" component={Dashboard} />
+            </Tab.Navigator>
+          ) : (
+            <View>
+              <SignIn />
+            </View>
+          )}
+        </ScreenContent>
+      </Container>
+    </View>
+  );
+}
