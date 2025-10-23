72c4a780dc2af89ab03fea8ea9ada521bc1edf9f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-23 15:16:49 +0200

    react-natived: fixing navigation

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -9,20 +9,29 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Dashboard from '@/components/Dashboard';
 import SignUp from '@/components/SignUp';
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Tab = createBottomTabNavigator();
+
 export default function Home() {  
   const [loggin, setLoggin] = useState(false);
+  useEffect(() => {
    const checkLogin = async() => {
-     const res = await axios.get('http://localhost:5000/loggedIn', { withCredentials: true });
+    await axios.get('http://localhost:5000/loggedIn', { withCredentials: true });
      setLoggin(true);
    }
+   checkLogin();
+  }, []);
+
   return (
          <View className='flex-1'>
              <Container>
                 <ScreenContent>
-                      <SignIn />
+                  {loggin ? (
+                    <Tab.Navigator initialRouteName='Home'>
+                        <Tab.Screen />
+                    </Tab.Navigator>
+                  )}
                 </ScreenContent>
                   </Container>
           </View>  
