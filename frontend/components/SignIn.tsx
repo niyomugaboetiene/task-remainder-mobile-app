4bca71dd1d05037490169c9b1a74d25e533bd416
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-23 10:19:53 +0200

    react-native: creating account  using frontend

--- frontend/components/SignIn.tsx
+++ frontend/components/SignIn.tsx
@@ -8,13 +8,19 @@ import axios from "axios";
    const [email, setEmail] = useState("");
    const [password, setPassword] = useState("");
    const [success, setSuccess] = useState("");
+   const [error, setMessage] = useState(null);
 
    const CreateAccount = async () => {
-    const res = await axios.post('http://localhost:5000/sign-in', {full_name, username, email, }, { withCredentials: true });
-    setSuccess(res.data.message);
-    setFull_name("");
-    setEmail("");
-    setPassword("");
-    setUsername("");
+    try {
+       const res = await axios.post('http://localhost:5000/sign-in', {full_name, username, email, }, { withCredentials: true });
+       setSuccess(res.data.message);
+       setFull_name("");
+       setEmail("");
+       setPassword("");
+       setUsername("");
+    } catch (error) {
+        setEmail("Unable to create Account");
+    }
+
    }
 }
