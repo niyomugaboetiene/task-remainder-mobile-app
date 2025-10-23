7244deb92fd549f0e2be937a2c3dd4873b32004e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-23 07:44:08 +0200

    react-native: fetching all task

--- frontend/components/TaskList.tsx
+++ frontend/components/TaskList.tsx
@@ -1,11 +1,23 @@
 import axios from "axios";
 import { View, Text } from "react-native"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function AllTask() {
+    const [tasks, setTasks] = useState({});
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false);
+
    useEffect(() => {
       const ListOfTask = async () => {
-        const result = await axios.get('http://localhost:5000/task/all', { withCredentials: true })
+        try {
+            setLoading(true);
+           const result = await axios.get('http://localhost:5000/task/all', { withCredentials: true });
+           setTasks(result);
+           setLoading(false);
+        } catch (error) {
+
+        }
+        
       }
    })
 }
