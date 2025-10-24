3fba10d7a690786fcc0d861a00db3001226c199d
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-24 08:48:35 +0200

    react-native: implementing dashboard

--- frontend/components/Dashboard.tsx
+++ frontend/components/Dashboard.tsx
@@ -33,8 +33,9 @@ function Dashboard(){
 
     return (
         <View className="min-h-screen bg-gradient-to-bl from-blue-300 to-gray-300 via-green-300 p-5">
-            <Text className="text-center font-light text-lg">{getGreeting()} {userInfo} Welcome to task remainder </Text>
-            <Text>Your Pending tasks</Text>
+            <Text className="text-center font-light text-lg">{getGreeting()} {userInfo}</Text>
+            <Text>Welcome to task remainder </Text>
+            <Text>Your Task</Text>
             <View>
                     <Text>Learing python</Text>
                     <Text>Learing python</Text>
