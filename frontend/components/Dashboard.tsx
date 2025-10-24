4423e220ced320e2907e5a9413b6e69b28f9aa06
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-24 13:55:13 +0200

    react-native: to implement sum of all task

--- frontend/components/Dashboard.tsx
+++ frontend/components/Dashboard.tsx
@@ -131,15 +131,22 @@ function Dashboard(){
                 <FlatList
                    data={userTasks}
                    keyExtractor={(item) => item.id.toString()}
-                   renderItem={({ item }) => (
-                        <View className="bg-white p-4 rounded-xl mt-4 shadow-xl">
-                           <Text>Title: {item.title}</Text>
-                           <Text>Description: {item.description}</Text>
-                           <Text>Finishing Date: {item.due_date}</Text>
-                           <Text>Status: {item.status}</Text>
-                           <Text>Created at: {item.created_at}</Text>
+                   renderItem={({ item }) => {
+                    const bgColor = 
+                       item.status === 'pending' ? "#FDE68A":
+                       item.status === 'completed' ? "#34D399":
+                       "#E5E7EB";
+                       return (
+                          <View className="bg-white p-4 rounded-xl mt-4 shadow-xl">
+                               <Text>Title: {item.title}</Text>
+                               <Text>Description: {item.description}</Text>
+                               <Text>Finishing Date: {item.due_date}</Text>
+                               <Text>Status: {item.status}</Text>
+                               <Text>Created at: {item.created_at}</Text>
                         </View>
-                   )}
+                       )
+       
+                   }}
                  / >
 
             </View>
